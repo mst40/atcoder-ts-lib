7d4d78207be56a7b3e159bc77c58ee6e4ee4932b
mst40 <35ma.ma.ma19@gmail.com>
2023-11-29 16:25:40 +0900

    lib add Combinations

---
+++ Combinations.ts
@@ -0,0 +1,16 @@
+function Combinations<T>(arr: T[], size: number): T[][] {
+    const res: T[][] = []
+    const len: number = arr.length
+    if (len < size) { return []; }
+
+    if (size === 1) { for (const a of arr) { res.push([a]) } }
+    else {
+        for (let i = 0; i < len; i++) {
+            const remain = Combinations(arr.slice(i + 1), size - 1)
+            for (const c of remain) {
+                res.push([arr[i], ...c])
+            }
+        }
+    }
+    return res
+}
