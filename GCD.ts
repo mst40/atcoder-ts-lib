f354c046d055d522666e333c072db2889febd551
mst40 <35ma.ma.ma19@gmail.com>
2023-12-05 21:11:58 +0900

    add comment

--- GCD.ts
+++ GCD.ts
@@ -1,3 +1,9 @@
 function GCD(x: number, y: number): number {
     return y ? GCD(y, x % y) : x
 }
+
+//以下のgcdは(1,0)のようにyが0のとき、1%0をするとNaNとなり、0を返してしまうので使用しないほうが良い。
+// function gcd(x, y) {
+//     if (x % y) { return gcd(y, x % y) }
+//     return y
+// }
