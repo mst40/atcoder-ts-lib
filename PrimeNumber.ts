a12595c7be6d854edcfab5abdeecbae8ac757d34
mst40 <35ma.ma.ma19@gmail.com>
2023-12-15 20:59:53 +0900

    lib add PrimeNumberList

---
+++ PrimeNumber.ts
@@ -0,0 +1,14 @@
+function PrimeNumberList<T>(num: number): number[] {
+    let list = new Array(num + 1).fill(1)
+    const res: number[] = []
+    for (let i = 2; i < num; i++) {
+        if (list[i]) {
+            res.push(i)
+            for (let j = 2; j <= Math.floor(num / i); j++) {
+                list[i * j] = 0
+            }
+        }
+    }
+
+    return res
+}
