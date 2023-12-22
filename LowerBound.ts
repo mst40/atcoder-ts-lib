3802db334fb9e60fe4adf7e2bb096d823b097ada
mst40 <35ma.ma.ma19@gmail.com>
2023-12-23 00:00:02 +0900

    lib add editorial

--- LowerBound.ts
+++ LowerBound.ts
@@ -1,4 +1,6 @@
-function LowerBound(sortedArray: number[], target: number) {
+// Return the index of smallest value greater than or equal to the target value. 
+//if the target value is largest, the last index is retruned.
+function LowerBound(sortedArray: number[], target: number): number {
     let low: number = 0;
     let high: number = sortedArray.length - 1
     let mid: number = 0
