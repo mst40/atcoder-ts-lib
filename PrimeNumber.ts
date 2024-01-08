90ea25ef7dc799f99e4572a8075bf8cd5aa35afe
mst40 <35ma.ma.ma19@gmail.com>
2024-01-08 17:27:02 +0900

    rename function

--- PrimeNumber.ts
+++ PrimeNumber.ts
@@ -1,14 +1,11 @@
-function PrimeNumberList<T>(num: number): number[] {
+function sieveOfEratosthenes<T>(num: number): number[] {
     let list = new Array(num + 1).fill(1)
     const res: number[] = []
     for (let i = 2; i < num; i++) {
         if (list[i]) {
             res.push(i)
-            for (let j = 2; j <= Math.floor(num / i); j++) {
-                list[i * j] = 0
-            }
+            for (let j = 2; j <= Math.floor(num / i); j++) { list[i * j] = 0 }
         }
     }
-
     return res
 }
