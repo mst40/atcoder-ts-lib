c0464f72b801fdcafe36bd298e9c9c42a4e513c8
mst40 <35ma.ma.ma19@gmail.com>
2024-03-18 18:41:06 +0900

    update: add Type  & rename value

--- PriorityQueue.ts
+++ PriorityQueue.ts
@@ -5,46 +5,46 @@ class PriorityQueue {
   }
 
   push(v: number) {
-    let ary = this.ary;
-    let i = ary.length++;
-    let j;
+    let ary: number[] = this.ary;
+    let i: number = ary.length++;
+    let mid: number = 0;
 
     while (i) {
-      j = (i - 1) >> 1;
-      if (ary[j] <= v) break;
-      ary[i] = ary[j];
-      i = j;
+      mid = (i - 1) >> 1;
+      if (ary[mid] <= v) break;
+      ary[i] = ary[mid];
+      i = mid;
     }
 
     ary[i] = v;
   }
 
-  pop() {
-    let ary = this.ary;
-    let top = ary[0];
-    let popped = ary.pop()!;
+  pop(): number {
+    let ary: number[] = this.ary;
+    let top: number = ary[0];
+    let popped: number = ary.pop()!;
 
-    let i = 0;
-    let mid = ary.length >> 1;
-    let j;
+    let i: number = 0;
+    let k: number = ary.length >> 1;
+    let mid: number = 0;
 
-    while (i < mid) {
-      j = i * 2 + 1;
-      if (ary[j + 1] < ary[j]) j++;
-      if (popped <= ary[j]) break;
-      ary[i] = ary[j];
-      i = j;
+    while (i < k) {
+      mid = i * 2 + 1;
+      if (ary[mid + 1] < ary[mid]) mid++;
+      if (popped <= ary[mid]) break;
+      ary[i] = ary[mid];
+      i = mid;
     }
 
     if (ary.length) ary[i] = popped;
     return top;
   }
 
-  size() {
+  size(): number {
     return this.ary.length;
   }
 
-  top() {
+  top(): number {
     return this.ary[0];
   }
 }
