17d0c1060894d5796b70770c1b3574336d58e960
mst40 <35ma.ma.ma19@gmail.com>
2024-03-18 18:32:38 +0900

    remake priority queue

--- PriorityQueue.ts
+++ PriorityQueue.ts
@@ -1,69 +1,50 @@
-//常に先頭が一番小さい
-
-class priorityQueue {
-  //queue:[data, node]
-  queue: { data: number; node: number }[];
-  //比較関数 最小(a,b)=>(a <= b) or 最大(a,b)=>(a >= b)
-  compare: (a: number, b: number) => boolean;
-
-  constructor(compare: (a: number, b: number) => boolean) {
-    this.queue = [];
-    this.compare = compare;
+class PriorityQueue {
+  ary: number[];
+  constructor() {
+    this.ary = [];
   }
 
-  push(d: number, n: number) {
-    const q = this.queue;
-    let i = q.length;
-    let mid = 0;
+  push(v: number) {
+    let ary = this.ary;
+    let i = ary.length++;
+    let j;
+
     while (i) {
-      mid = (i - 1) >> 1;
-
-      if (this.compare(q[mid].data, d)) {
-        break;
-      }
-
-      q[i] = q[mid];
-      i = mid;
+      j = (i - 1) >> 1;
+      if (ary[j] <= v) break;
+      ary[i] = ary[j];
+      i = j;
     }
-    q[i] = { data: d, node: n };
+
+    ary[i] = v;
   }
 
-  pop(): { data: number; node: number } | undefined {
-    if (!this.queue) {
-      return undefined;
-    }
-
-    const q = this.queue;
-    const r = q[0];
-    const popQ = q.pop()!;
-    const k = q.length >> 1;
+  pop() {
+    let ary = this.ary;
+    let top = ary[0];
+    let popped = ary.pop()!;
 
     let i = 0;
-    let mid = 0;
-    while (i < k) {
-      mid = (i << 1) + 1;
-      if (q[mid + 1] && q[mid + 1].data < q[mid].data) {
-        mid++;
-      }
+    let mid = ary.length >> 1;
+    let j;
 
-      if (this.compare(popQ.data, q[mid].data)) {
-        break;
-      }
-
-      q[i] = q[mid];
-      i++
+    while (i < mid) {
+      j = i * 2 + 1;
+      if (ary[j + 1] < ary[j]) j++;
+      if (popped <= ary[j]) break;
+      ary[i] = ary[j];
+      i = j;
     }
 
-    if (q.length) {
-      q[i] = popQ;
-    }
+    if (ary.length) ary[i] = popped;
+    return top;
+  }
 
-    return r;
+  size() {
+    return this.ary.length;
   }
-  get size() {
-    return this.queue.length;
-  }
-  get top() {
-    return this.queue[0];
+
+  top() {
+    return this.ary[0];
   }
 }
