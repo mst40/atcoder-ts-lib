5e31cf10ef486e4c0b55f67c91097f765fe53cae
mst40 <35ma.ma.ma19@gmail.com>
2024-07-21 00:53:37 +0900

    update lib: Permutation

--- Permutation.ts
+++ Permutation.ts
@@ -1,23 +1,37 @@
-function nextPermutation<T>(arr: T[], size: number): T[][] {
-    const res: T[][] = [];
-    const len: number = arr.length
+class Permutation<T> {
+  private ary: T[];
+  constructor(ary: T[]) {
+    this.ary = ary.sort();
+  }
 
-    if (len < size) { return [] }
-
-    if (size === 1) {
-        for (let i = 0; i < len; i++) {
-            res.push([arr[i]])
-        }
-    } else {
-        for (let i = 0; i < len; i++) {
-            let remain = [...arr]
-            remain.splice(i, 1)
-
-            let perms = nextPermutation(remain, size - 1)
-            for (const p of perms) {
-                res.push([arr[i], ...p])
-            }
-        }
+  nextPermutation(ary: T[]) {
+    let i = ary.length - 2;
+    while (i >= 0 && ary[i] >= ary[i + 1]) {
+      i--;
     }
-    return res
+    if (i < 0) return false;
+
+    let j = ary.length - 1;
+    while (ary[j] <= ary[i]) {
+      j--;
+    }
+    [ary[i], ary[j]] = [ary[j], ary[i]];
+
+    let left = i + 1;
+    let right = ary.length - 1;
+    while (left < right) {
+      [ary[left], ary[right]] = [ary[right], ary[left]];
+      left++;
+      right--;
+    }
+
+    return true;
+  }
+
+  allPerms(): T[][] {
+    const res: T[][] = [];
+    do res.push([...this.ary]);
+    while (this.nextPermutation(this.ary));
+    return res;
+  }
 }
