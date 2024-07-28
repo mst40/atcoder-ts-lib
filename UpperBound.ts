40b9e02435cb91499b1720b5d999b9f6b76c4c59
mst40 <35ma.ma.ma19@gmail.com>
2024-07-28 23:10:55 +0900

    Update lib: UpperBound.ts

--- UpperBound.ts
+++ UpperBound.ts
@@ -1,18 +1,14 @@
-// Returns the index of smallest value that is greater than the target value. (target < value[index])
 function UpperBound(sortedArray: number[], target: number): number {
     let low: number = 0;
-    let high: number = sortedArray.length - 1
-    let mid: number = 0
-    let res = 0
+    let high: number = sortedArray.length - 1;
+    let mid: number = 0;
     while (low <= high) {
-        mid = Math.floor((low + high) / 2)
-        if (sortedArray[mid] < target) { low = mid + 1; }
-        else if (sortedArray[mid] > target) { high = mid - 1 }
-        else {
-            res = mid + 1
-            break
-        }
+      mid = Math.floor((low + high) / 2);
+      if (sortedArray[mid] <= target) {
+        low = mid + 1;
+      } else {
+        high = mid - 1;
+      }
     }
-    if (res === 0) { res = low }
-    return res
-}
+    return low;
+  }
