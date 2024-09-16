f49c804c38d348271deb4d6db3d09b56e6a06e9a
mst40 <35ma.ma.ma19@gmail.com>
2024-09-16 20:24:06 +0900

    Update lib: update union-find
    update getParent & getSize

--- union-find.ts
+++ union-find.ts
@@ -1,31 +1,37 @@
 
 class UnionFind {
-    private parent: number[]
-    private size: number[]
+    private parent: number[];
+    private size: number[];
 
     constructor(arrLen: number) {
-        this.parent = new Array<number>(arrLen + 1).fill(0).map((v, i) => i)
-        this.size = new Array<number>(arrLen + 1).fill(1)
+      this.parent = new Array<number>(arrLen + 1).fill(0).map((v, i) => i);
+      this.size = new Array<number>(arrLen + 1).fill(1);
     }
 
     private find(target: number): number {
-        if (target !== this.parent[target]) {
-            return this.parent[target] = this.find(this.parent[target])
-        }
-        return target
+      if (target !== this.parent[target]) {
+        return (this.parent[target] = this.find(this.parent[target]));
+      }
+      return target;
     }
 
     public unite(a: number, b: number): void {
-        let [ap, bp] = [this.find(a), this.find(b)]
-        if (ap !== bp) {
-            if (this.size[ap] < this.size[bp]) { [ap, bp] = [bp, ap] }
-            this.parent[bp] = ap;
-            this.size[ap] += this.size[bp]
+      let [ap, bp] = [this.find(a), this.find(b)];
+      if (ap !== bp) {
+        if (this.size[ap] < this.size[bp]) {
+          [ap, bp] = [bp, ap];
         }
+        this.parent[bp] = ap;
+        this.size[ap] += this.size[bp];
+      }
     }
 
-    get getParent() { return this.parent }
+    public getParent(v: number) {
+      return this.find(v);
+    }
 
-    get getSize() { return this.size }
-}
+    public getSize(v: number) {
+      return this.size[this.find(v)];
+    }
+  }
 
