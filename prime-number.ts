3296647093532659c9b0eeeb64130e7451922eb7
mst40 <35ma.ma.ma19@gmail.com>
2024-09-30 18:18:34 +0900

    Update lib: Add prime facotrize func to lib/prime-number

--- prime-number.ts
+++ prime-number.ts
@@ -1,11 +1,30 @@
-function sieveOfEratosthenes<T>(num: number): number[] {
-    let list = new Array(num + 1).fill(1)
-    const res: number[] = []
-    for (let i = 2; i <= num; i++) {
-        if (list[i]) {
-            res.push(i)
-            for (let j = 2; j <= Math.floor(num / i); j++) { list[i * j] = 0 }
-        }
+// return a prime number list of values up to N
+// O(N)
+function sieveOfEratosthenes(N: number): number[] {
+  let list: boolean[] = new Array(N + 1).fill(true);
+  const res: number[] = [];
+  for (let i = 2; i <= N; i++) {
+    if (list[i]) {
+      res.push(i);
+      for (let j = 2; j <= Math.floor(N / i); j++) {
+        list[i * j] = false;
+      }
     }
-    return res
+  }
+  return res;
+}
+
+// return prime factor list of N
+// O(logN)
+function primeFactorize(v: number): number[] {
+  const res: number[] = [];
+  for (let i = 2; i * i <= v; i++) {
+    if (v % i) continue;
+    res.push(i);
+    while (v % i == 0) {
+      v /= i;
+    }
+  }
+  if (v !== 1) res.push(v);
+  return res;
 }
