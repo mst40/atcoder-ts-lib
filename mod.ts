092a8507db5181f08a1fa7e6973ef951b05c929a
mst40 <35ma.ma.ma19@gmail.com>
2024-12-18 09:02:10 +0900

    Update lib: mod
    add Mod Object

--- mod.ts
+++ mod.ts
@@ -37,6 +37,41 @@ class Mod {
   }
 }
 
+const mod = {
+  mod: 10n ** 9n + 7n,
+  mul(a: bigint, b: bigint) {
+    return (a * b) % this.mod;
+  },
+
+  // a ** n % mod
+  pow(a: bigint, n: bigint): bigint {
+    if (n === 0n) {
+      return 1n;
+    }
+
+    if (n % 2n === 1n) {
+      // odd
+      return this.mul(a, this.pow(a, n - 1n));
+    } else {
+      // even
+      return this.pow(this.mul(a, a), n / 2n);
+    }
+  },
+
+  // a の逆元(inverse element)
+  // a * inv(a) = 1
+  //*1
+  inv(a: bigint) {
+    return this.pow(a, this.mod - 2n);
+  },
+
+  // p/q % mod
+  // 逆元があるときのみ有効
+  div(p: bigint, q: bigint) {
+    return this.mul(p, this.inv(q));
+  },
+};
+
 /**
  * *1
  * b = a**(m-2)%m,   a*b%m = 1
